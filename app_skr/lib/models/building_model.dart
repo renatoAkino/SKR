477b59d10330d1a2ee83d6f00bdc697b75fb60c2
renatoAkino <renato.aquino@etec.sp.gov.br>
2021-04-15 03:37:29 -0300

    Criando o projeto flutter e o model dos edificios

---
+++ app_skr/lib/models/building_model.dart
@@ -0,0 +1,42 @@
+
+
+class buildingModel {
+  int idBuilding;
+  String titleBuilding;
+  String typeBuilding;
+  String descBuilding;
+  String categoryBuilding;
+  String lATbuilding;
+  String lONGbuilding;
+
+  buildingModel(
+      {this.idBuilding,
+        this.titleBuilding,
+        this.typeBuilding,
+        this.descBuilding,
+        this.categoryBuilding,
+        this.lATbuilding,
+        this.lONGbuilding});
+
+  buildingModel.fromJson(Map<String, dynamic> json) {
+    idBuilding = json['idBuilding'];
+    titleBuilding = json['titleBuilding'];
+    typeBuilding = json['typeBuilding'];
+    descBuilding = json['descBuilding'];
+    categoryBuilding = json['categoryBuilding'];
+    lATbuilding = json['LATbuilding'];
+    lONGbuilding = json['LONGbuilding'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['idBuilding'] = this.idBuilding;
+    data['titleBuilding'] = this.titleBuilding;
+    data['typeBuilding'] = this.typeBuilding;
+    data['descBulding'] = this.descBuilding;
+    data['categoryBuilding'] = this.categoryBuilding;
+    data['LATbuilding'] = this.lATbuilding;
+    data['LONGbuilding'] = this.lONGbuilding;
+    return data;
+  }
+}
